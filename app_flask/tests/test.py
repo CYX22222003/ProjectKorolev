9428493f8c6186e3cfbb1efdace4727a532da39b
guest1@CHENYIXUN <you@example.com>
2024-05-19 10:24:48 +0800

    add unit test for API

---
+++ app_flask/tests/test.py
@@ -0,0 +1,21 @@
+import unittest
+from application import app
+import os
+from dotenv import load_dotenv
+
+class FlaskTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+        load_dotenv()
+        self.headers = headers = {
+            "API-Key" : os.getenv("USER_KEY")
+        }
+
+    def test_index(self): 
+        response = self.app.get("/",headers = self.headers)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
