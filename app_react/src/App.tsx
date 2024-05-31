fef498fb174cf434321670888d5a7134401b4c10
guest1@CHENYIXUN <you@example.com>
2024-05-31 18:21:09 +0800

    Update routing of frontend

--- app_react/src/App.tsx
+++ app_react/src/App.tsx
@@ -52,7 +52,6 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/test" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </AuthenContext.Provider>
   );
