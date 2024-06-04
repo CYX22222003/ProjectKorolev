f1ed3e7414902fa389cda540d90ff923334b82b9
guest1@CHENYIXUN <you@example.com>
2024-06-04 10:48:15 +0800

    Create branch for patient management

--- app_react/src/Homepage/Welcome.tsx
+++ app_react/src/Homepage/Welcome.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
+import React, {ReactElement} from "react";
 import Background from "../Components/Background";
 import WelcomeHelp from "./WelcomeHelp";
 
-export default function Welcome() {
+export default function Welcome() : ReactElement {
   return <Background element={<WelcomeHelp />} header="Home" />;
 }
