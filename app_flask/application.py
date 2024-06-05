a081c68478d2a0d85386a5e84b36e84156021943
guest1@CHENYIXUN <you@example.com>
2024-06-05 21:40:30 +0800

    Update Flask backend - attempt to implement simple session and patient management functions

--- app_flask/application.py
+++ app_flask/application.py
@@ -1,53 +1,72 @@
-from flask import Flask,request,jsonify, session, redirect, url_for, Response
+from flask import Flask, request, jsonify, session, redirect, url_for, Response
 from flask_cors import CORS
-from src.models import db, User
+from src.models import db, User, Patient, Session
 import json
 import os
 from dotenv import load_dotenv
-from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+from flask_login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    current_user,
+    login_required,
+)
+
 load_dotenv()
 
 app = Flask(__name__)
 wsgi_app = app.wsgi_app
-app.config['SECRET_KEY'] = 'YOUR-SECRET'
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["SECRET_KEY"] = "YOUR-SECRET"
+app.config["CORS_HEADERS"] = "Content-Type"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
-app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-app.config['SESSION_COOKIE_SECURE'] = 'true'
-CORS(app, supports_credentials=True, resources=r'/*')
+app.config["SESSION_COOKIE_SAMESITE"] = "None"
+app.config["SESSION_COOKIE_SECURE"] = "true"
+CORS(app, supports_credentials=True, resources=r"/*")
 
 db.init_app(app)
 with app.app_context():
     db.drop_all()
     db.create_all()
 
-#simple API key generation
-dummy_db = {os.getenv("USER_KEY"): 'user1'}
+# simple API key generation
+dummy_db = {os.getenv("USER_KEY"): "user1"}
+
 
 def authenticate_api_key(api_key):
     return dummy_db.get(api_key)
+
 
 @app.before_request
 def before_request():
     if request.method != "OPTIONS":
         request.get_data()
-        api_key = request.headers.get('Authorization')
+        api_key = request.headers.get("Authorization")
         if not api_key or not authenticate_api_key(api_key):
-            return jsonify({'error': 'Unauthorized'}), 401
+            return jsonify({"error": "Unauthorized"}), 401
 
-#default routes for testing
-@app.route('/')
+
+# default routes for testing
+@app.route("/")
 def hello_world():
-    return 'Hello from Flask!'
+    return "Hello from Flask!"
 
-#SQLAchemy ORM for basic CRUD execution + User registration
+
+# user management
 @app.route("/users")
 def user_list():
     users = db.session.execute(db.select(User).order_by(User.username)).scalars()
 
-    return str(list(map(lambda x: f"username: {x.username}, email : {x.email}", list(users.fetchall()))))
+    return str(
+        list(
+            map(
+                lambda x: f"username: {x.username}, email : {x.email}",
+                list(users.fetchall()),
+            )
+        )
+    )
 
-@app.route("/users/create", methods=["GET", "POST"])
+
+@app.route("/user/create", methods=["GET", "POST"])
 def user_create():
     if request.method == "POST":
         request.get_data()
@@ -56,16 +75,24 @@ def user_create():
         user = User(
             username=result["username"],
             email=result["email"],
-            password=result["password"]
+            password=result["password"],
         )
         db.session.add(user)
         db.session.commit()
-        return Response(f"username: {user.username}, email: {user.email}, id: {user.id}", content_type='text/plain')
-    
+        return Response(
+            f"username: {user.username}, email: {user.email}, id: {user.id}",
+            content_type="text/plain",
+        )
+
+
 @app.route("/user/<int:id>")
 def user_detail(id):
     user = db.get_or_404(User, id)
-    return Response(f"username: {user.username}, email: {user.email}, id: {user.id}", content_type="text/plain")
+    return Response(
+        f"username: {user.username}, email: {user.email}, id: {user.id}",
+        content_type="text/plain",
+    )
+
 
 @app.route("/user/<int:id>/delete", methods=["GET", "POST", "DELETE"])
 def user_delete(id):
@@ -78,54 +105,128 @@ def user_delete(id):
 
     return "unable to delete the content"
 
-#user login and registration in Flask
+
+# user login and registration in Flask
 login_manager = LoginManager()
 login_manager.init_app(app)
+
 
 @login_manager.user_loader
 def user_loader(username):
     return db.session.execute(db.select(User).filter_by(username=username)).scalar_one()
 
-@app.route('/loginTest', methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login_test():
     if request.method == "POST":
         request.get_data()
         result = json.loads(request.data.decode())
         username = result["username"]
         passwd = result["passwd"]
-        user = db.session.execute(db.select(User).filter_by(username=username)).scalar_one()
+        user = db.session.execute(
+            db.select(User).filter_by(username=username)
+        ).scalar_one()
 
         if user == None:
             return "unable to login"
-        
+
         if not user.is_active:
             return "inactive account"
         if not user.verify_password(passwd):
             return "wrong password or username"
-        
+
         login_user(user)
         return f"{current_user.get_id()} logged in"
     return "errors occured"
 
-@app.route('/welcome', methods=['GET'])
+
+@app.route("/welcome", methods=["GET"])
 @login_required
-def welcome():    
+def welcome():
     return f"welcome {current_user.get_id()}. Login is successful"
 
 
-@app.route('/logoutTest', methods=['POST'])
+@app.route("/logout", methods=["POST"])
 @login_required
 def logout():
     logout_user()
     return "logged out successfully"
 
+
+# patient management
+@app.route("/patients", methods=["GET"])
+@login_required
+def show_all_patients():
+    order = db.select(Patient).filter_by(user_id=current_user.show_id())
+    patients = db.session.execute(order).scalars()
+    iter = list(patients.fetchall())
+    return jsonify({"list": list(map(lambda x: (x.name, x.patient_id), iter))})
+
+
+@app.route("/patient/create", methods=["POST"])
+@login_required
+def create_new_patient():
+    if request.method == "POST":
+        request.get_data()
+        result = json.loads(request.data.decode())
+        patient = Patient(name=result["patient_name"], user_id=current_user.show_id())
+        db.session.add(patient)
+        db.session.commit()
+        return "patient is successfully created"
+
+
+@app.route("/patient/<int:patient_id>/delete", methods=["DELETE"])
+@login_required
+def delete_patient(patient_id):
+    patient = db.get_or_404(Patient, patient_id)
+    if request.method == "DELETE":
+        db.session.delete(patient)
+        db.session.commit()
+        return "successful delete"
+
+
+# session management
+@app.route("/session/create", methods=["POST"])
+@login_required
+def create_new_session():
+    if request.method == "POST":
+        request.get_data()
+        result = json.loads(request.data.decode())
+        new_session = Session(
+            session_name=result["session_name"],
+            patient_id=result["patient_id"],
+            user_id=current_user.show_id(),
+        )
+        db.session.add(new_session)
+        db.session.commit()
+        return "session is successfully created"
+
+
+@app.route("/sessions", methods=["GET"])
+@login_required
+def show_sessions():
+    order = db.select(Session).filter_by(user_id=current_user.show_id())
+    sessions = db.session.execute(order).scalars()
+    return jsonify(
+        {
+            "collections": list(
+                map(
+                    lambda x: [x.session_name, x.patient_id, x.user_id],
+                    list(sessions.fetchall()),
+                )
+            )
+        }
+    )
+
+
 @app.after_request
 def apply_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, DELETE'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS, PUT, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     return response
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
--- app_flask/src/models.py
+++ app_flask/src/models.py
@@ -2,21 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from flask_restful import Resource, reqparse
+
 
 class Base(DeclarativeBase):
     pass
 
+
 db = SQLAlchemy(model_class=Base)
 
+
 class User(db.Model):
-    __tablename__ = 'user_table'
+    __tablename__ = "user_table"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str]
     patients: Mapped[list["Patient"]] = relationship()
+    sessions: Mapped[list["Session"]] = relationship()
 
     def __init__(self, username, password, email):
         self.password = password
@@ -26,24 +29,24 @@ class User(db.Model):
     @property
     def is_authenticated(self):
         return True
-    
+
     @property
     def is_anonymous(self):
         return False
-    
+
     @property
     def is_active(self):
         return True
-    
+
     def get_id(self):
         return self.username
-    
+
     def verify_password(self, password_in):
         return password_in == self.password
-    
+
     def show_id(self):
         return self.id
-        
+
 
 class Patient(db.Model):
     __tablename__ = "patient_table"
@@ -56,26 +59,28 @@ class Patient(db.Model):
     def __init__(self, name, user_id):
         self.name = name
         self.user_id = user_id
-    
-    def serialize(self):
-        return {
-            'patient_id' : self.id,
-            'name' : self.name,
-        }
 
 
 class Session(db.Model):
     __tablename__ = "session_table"
 
     session_id: Mapped[int] = mapped_column(primary_key=True)
+    session_name: Mapped[str] = mapped_column(nullable=False)
     patient_id: Mapped[int] = mapped_column(ForeignKey("patient_table.patient_id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_table.id"))
+
+    def __init__(self, session_name, patient_id, user_id):
+        self.session_name = session_name
+        self.patient_id = patient_id
+        self.user_id = user_id
 
 
 def database_test():
     test = Flask(__name__)
-    test.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    test.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
     db.init_app(test)
     with test.app_context():
+        db.drop_all()
         db.create_all()
         new_user = User(username="test", password="test", email="test@test.com")
         db.session.add(new_user)
@@ -85,11 +90,22 @@ def database_test():
         db.session.add(new_patient)
         db.session.commit()
 
+        new_session = Session(
+            session_name=f"{new_patient.name}_5/6/2024",
+            patient_id=new_patient.patient_id,
+            user_id=new_patient.user_id,
+        )
+        db.session.add(new_session)
+        db.session.commit()
+
         users = db.session.execute(db.select(User)).scalars()
         patients = db.session.execute(db.select(Patient)).scalars()
+        sessions = db.session.execute(db.select(Session)).scalars()
+
         out1 = users.fetchall()[0] == new_user
         out2 = patients.fetchall()[0] == new_patient
+        out3 = sessions.fetchall()[0] == new_session
         db.session.commit()
         db.session()
         db.drop_all()
-    return out1 and out2
+    return (out1 and out2) and out3
