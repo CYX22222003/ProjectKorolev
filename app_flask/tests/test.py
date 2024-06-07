384e27b4df45fc320a421cdc1b62be4bf44c44bc
guest1@CHENYIXUN <you@example.com>
2024-06-07 23:58:26 +0800

    Correct bugs in actions yml

--- app_flask/tests/test.py
+++ app_flask/tests/test.py
@@ -4,21 +4,22 @@ import os
 from dotenv import load_dotenv
 from src.models import database_test
 
-
 class FlaskTest(unittest.TestCase):
-    # configure for unit test
+    # configure for github test
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
         load_dotenv()
-        self.headers = {"Authorization": os.getenv("USER_KEY")}
+        self.headers = {
+            "Authorization" : os.getenv("USER_KEY")
+        }
 
-    # simple tests for status code
-    def test_index(self):
-        response = self.app.get("/", headers=self.headers)
+    #simple tests for status code
+    def test_index(self): 
+        response = self.app.get("/",headers = self.headers)
         self.assertEqual(response.status_code, 200)
 
-    # simple db_test
+    #simple db_test
     def test_db(self):
         self.assertEqual(database_test(), True)
 
