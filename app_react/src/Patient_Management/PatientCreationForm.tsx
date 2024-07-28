75d75c0ea085952a5601d92da8f73946f5ad7472
Golddirio <142647056+Golddirio@users.noreply.github.com>
2024-07-28 22:39:07 +0800

    Update PatientCreationForm.tsx

--- app_react/src/Patient_Management/PatientCreationForm.tsx
+++ app_react/src/Patient_Management/PatientCreationForm.tsx
@@ -72,14 +72,15 @@ export default function PatientCreationForm({
                 />
               </Grid>
             </Grid>
+            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 3, mb: 2 }}>
             <Button
               type="submit"
-              fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+              sx={{borderRadius: "20px", width: "90px" }}
             >
               Create
             </Button>
+            </Box>
           </Box>
         </Box>
       </Container>
